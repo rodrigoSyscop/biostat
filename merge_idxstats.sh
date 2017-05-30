dab3c313d110c3f0ec111a8eb55c688ee1f5fa05
rodrigoSyscop <rodrigodelimavieira@gmail.com>
2017-05-30 10:40:18 -0300

    Fix line reading
    - reading line by line from idxstats files was not working. I had to replace the for loop by a while loop.
    - Also if the output file already exists the script will stop its execution.

--- merge_idxstats.sh
+++ merge_idxstats.sh
@@ -9,11 +9,16 @@
 for idx_file in $(ls *_sorted.txt);do
     echo "Working on $idx_file"
     index_genome=$(basename $idx_file)
-    index_genome=$(echo $index_genome |cut -d'.' -f1)
-    genome=$(echo $index_genome |cut -d'_' -f2,3)
+    index_genome=$(echo $index_genome |cut -d'.' -f1) # should store: 230_GC_Arun4_sorted
+    genome=$(echo $index_genome |cut -d'_' -f2,3) # # should store: GC_Arun4
     
+    # check if output file already exists
+    [ -f $genome.merged_idxstats.txt ] && echo "$genome.merged_idxstats.txt exists! Aborting..." && exit 1
+
     # for each line in each idxstats file
-    for line in $(cat $idx_file);do
+    while read line;do
         echo "$index_genome $line" >> $genome.merged_idxstats.txt
-    done
+    done < $idx_file
 done
+
+echo "All set on $genome.merged_idxstats.txt"
